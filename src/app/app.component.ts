a0ea62cb743a9b764b0a3271223ea0d0fa9e3523
Rei Medeiros <reimedeiros2010@hotmail.com>
2023-11-09 22:51:43 -0300

    Configuração inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { HeaderComponent } from "./components/header/header.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent]
+})
+export class AppComponent {
+  title = 'controle-compras-front';
+}
